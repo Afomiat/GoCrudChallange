cd311853f9f4cbc72e4df09581f0151764c609e4
Afomia Tadesse <afomiat79@gmail.com>
2024-10-27 16:15:57 -0700

    Finalize Person CRUD

--- Delivery/main.go
+++ Delivery/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"github.com/Afomiat/GoCrudChallange/Delivery/routers"
+	"net/http"
+
 	"github.com/Afomiat/GoCrudChallange/Database"
+	"github.com/Afomiat/GoCrudChallange/Delivery/routers"
+	"github.com/gorilla/handlers"
 )
 func main() {
 	Database.DBconnection()
 	router := routers.SetupRouter()
 
-	router.Run(":8080")
+	allowedOrigins := []string{"http://localhost:5173"}
+
+    corsHandler := handlers.CORS(
+        handlers.AllowedOrigins(allowedOrigins),
+        handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+        handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+    )(router)
+
+    http.ListenAndServe(":8080", corsHandler)
 }
